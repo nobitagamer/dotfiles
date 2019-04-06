07c74c21c635a357662e57d5ede414ddddfa504a
Nguyen Khac Trieu <trieunk@yahoo.com>
2019-04-06 17:13:52 +0700

    feat: Allow specific patch version when install Unity3D

--- apps/Unity3D/unity-install.sh
+++ apps/Unity3D/unity-install.sh
@@ -3,14 +3,20 @@
 cd ~/.Unity3D
 
 if [ -z "$1" ]; then
-    echo -e "\nPlease call '$0 <Unity3D version>' to run this command!\n"
-    exit 1
+  echo -e "\nPlease call '$0 <major>.<minor> [{patch}{type}{build}]' to run this command!\n"
+  exit 1
 fi
 
-cp ./config.json "~/Library/Application Support/install-unity/"
+if ! [ -z "$2" ]; then
+  UNITY_PATCH=.$2
+fi
+
+UNITY_CONFIG_PATH=~/Library/Application\ Support/install-unity/
+mkdir -p "$UNITY_CONFIG_PATH"
+cp ./config.json "$UNITY_CONFIG_PATH"
 
 UNITY_VERSION=$1
-sudo ~/install-unity install -y -p Unity,iOS,Android "$UNITY_VERSION"
+sudo ~/install-unity install -y -p Unity,iOS,Android "$UNITY_VERSION$UNITY_PATCH"
 
 sudo ./unity-patcher "/Applications/Unity $UNITY_VERSION/Unity.app"
-cp ./Unity_lic.ulf "/Applications/Unity $UNITY_VERSION/"
+cp ./Unity_lic.ulf "/Library/Application Support/Unity/"
