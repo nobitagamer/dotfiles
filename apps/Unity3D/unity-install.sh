77b9ba7ad9f948024042faf5ea6a366a5331d422
Nguyen Khac Trieu <trieunk@yahoo.com>
2019-04-06 16:25:00 +0700

    fix: Do not install VisualStudio package that requires accept EULA

--- apps/Unity3D/unity-install.sh
+++ apps/Unity3D/unity-install.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 fi
 
 UNITY_VERSION=$1
-sudo ~/install-unity install -y -p Unity,VisualStudio,iOS,Android "$UNITY_VERSION"
+sudo ~/install-unity install -y -p Unity,iOS,Android "$UNITY_VERSION"
 
 sudo ./unity-patcher "/Applications/Unity $UNITY_VERSION/Unity.app"
 cp ./Unity_lic.ulf "/Applications/Unity $UNITY_VERSION/"
