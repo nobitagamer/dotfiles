6e7d60390beaa1b0fd729d0166cece21213cbd17
Nguyen Khac Trieu <trieunk@yahoo.com>
2024-05-14 10:10:59 +0700

    feat: Install Rosetta on Apple Silicon Mx chips (arm64)

--- osx-install-xcode-command-line.sh
+++ osx-install-xcode-command-line.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -l
+#!/usr/bin/env bash
 
 # NOTE:
 # - Run System Update FIRST to fix https://forums.developer.apple.com/thread/84593
@@ -7,13 +7,14 @@
 # Skip if already install
 UPDATE_FILE=~/update-dotfiles.sh
 if [ ! -f $UPDATE_FILE ]; then
-  # Install all updates that are recommended for your system
+  # Install all updates that are recommended for your system (require `sudo` to use restart -r)
   sudo softwareupdate -i -r
 
-  # Install all updates that are applicable to your system,
-  # sudo softwareupdate -i -a
-
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  if uname -a| grep -q arm64; then
+    sudo softwareupdate --install-rosetta --agree-to-license
+  fi
+
   PROD=$(softwareupdate -l |
     grep "\*.*Command Line" |
     head -n 1 | awk -F"*" '{print $2}' |
